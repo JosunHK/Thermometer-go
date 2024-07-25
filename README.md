08859db0465c69be8de9c36e9f932ea0553331cc
Josun <dennistai.mail@gmail.com>
2024-07-26 02:27:01 +0800

    6x6 done

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Thermometer-go
+
+JS script
+
+let cells = [];
+document.querySelectorAll('.cell-off').forEach((cell) => {
+let clsName = cell.className
+let classes = clsName.split(' ')
+cells.push(classes[classes.length-2] + " " + Math.floor(cell.style.top.split("px")[0] / 30) + " " + Math.floor(cell.style.left.split("px")[0] / 30))
+})
+let s1 = cells.join(',')
+let vs = [];
+document.querySelectorAll('.task.v').forEach((v) => {
+vs.push(v.innerText)
+})
+let s2 = vs.join(',')
+let hs = [];
+document.querySelectorAll('.task.h').forEach((h) => {
+hs.push(h.innerText)
+})
+let s3 = hs.join(',')
+console.log(s1+";"+s2+";"+s3+";")
--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"slices"
+	"strconv"
+	"strings"
 
 	"github.com/golang-collections/collections/stack"
 )
@@ -14,8 +17,8 @@ type Node struct {
 }
 
 type Points struct {
-	x int
 	y int
+	x int
 }
 
 type r struct {
@@ -23,35 +26,24 @@ type r struct {
 	to   Points
 }
 
-var heads = []Points{
-	{0, 2},
-	{1, 0},
-	{1, 1},
-	{1, 2},
-	{2, 3},
-	{3, 3},
-}
+const raw = "start 0 4,straight 0 3,straight 0 2,straight 0 1,curve 0 0,curve 1 0,end 1 1,start 0 5,straight 1 5,straight 2 5,straight 3 5,straight 4 5,curve 5 5,end 5 4,start 1 2,straight 1 3,curve 1 4,straight 2 4,straight 3 4,end 4 4,start 2 0,curve 3 0,straight 3 1,end 3 2,start 2 1,straight 2 2,curve 2 3,end 3 3,start 4 0,curve 5 0,curve 5 1,end 4 1,start 4 2,curve 5 2,curve 5 3,end 4 3;3,1,3,2,4,4;3,4,3,2,2,3;"
 
-var relations = []r{
-	{Points{0, 2}, Points{0, 1}},
-	{Points{0, 1}, Points{0, 0}},
-	{Points{1, 0}, Points{2, 0}},
-	{Points{2, 0}, Points{3, 0}},
-	{Points{1, 1}, Points{2, 1}},
-	{Points{2, 1}, Points{3, 1}},
-	{Points{1, 2}, Points{2, 2}},
-	{Points{2, 3}, Points{1, 3}},
-	{Points{1, 3}, Points{0, 3}},
-	{Points{1, 3}, Points{0, 3}},
-	{Points{3, 3}, Points{3, 2}},
-}
+var heads []Points
+
+var relations []r
+
+var N int
 
 var m [][]*Node
-var targetX = []int{2, 2, 2, 1}
-var targetY = []int{2, 2, 1, 2}
+var nodeStack = stack.New()
+var currentX []int
+var currentY []int
+
+var targetX []int
+var targetY []int
 
 func main() {
-	const N int = 4
+	initData()
 	m = make([][]*Node, N)
 	for i := range m {
 		m[i] = make([]*Node, N)
@@ -69,80 +61,178 @@ func main() {
 		m[r.to.y][r.to.x] = next
 	}
 
-	// play(N)
+	printMap()
 
-	printMap(m)
+	if err := play(); err != nil {
+		fmt.Println(err)
+		printMap()
+	} else {
+		fmt.Println("GG")
+		printMap()
+	}
+
 }
 
-func play(N int) error {
-	nodeStack := stack.New()
-	currentX := make([]int, N)
-	currentY := make([]int, N)
-
-	fmt.Println(exhaustNode(m[2][1], currentX, currentY))
-
-	for i, h1 := range heads {
-		curr := m[h1.y][h1.x]
+func play() error {
+	for i, h := range heads {
+		headsToCheck := []Points{}
+		headsToCheck = append(headsToCheck, heads[:i]...)
+		headsToCheck = append(headsToCheck, heads[i+1:]...)
+		currentX = make([]int, N)
+		currentY = make([]int, N)
+		curr := m[h.y][h.x]
 		ptr := 0
 		for {
-			//back track
-			if curr == nil {
-				nodeStack.Pop()
-				//func decrement curr node(curr, currentX, currentY)
-				curr = nodeStack.Peek().(*Node).prev
-				ptr--
-				if ptr == i {
-					ptr--
+			eNode := exhaustNode(curr)
+			if eNode != nil {
+				nodeStack.Push(eNode)
+			}
+
+			if isGG() {
+				return nil
+			}
+
+			if ptr >= len(headsToCheck) {
+				disableNode(nodeStack.Peek().(*Node))
+				prev := nodeStack.Peek().(*Node).prev
+				if prev != nil {
+					nodeStack.Pop()
+					nodeStack.Push(prev)
+
+					head := getHead(prev)
+					ptr = slices.Index(headsToCheck, head.Points) + 1
+					if !(ptr >= len(headsToCheck)) {
+						curr = m[headsToCheck[ptr].y][headsToCheck[ptr].x]
+						continue
+					}
 				}
-			}
 
-			//end reached, start back track
-			if ptr == N {
-				curr = curr.prev
-			}
+				head := nodeStack.Pop().(*Node)
+				disableNode(head)
 
-			eNode := exhaustNode(curr, currentX, currentY)
-			nodeStack.Push(eNode)
+				if nodeStack.Len() == 0 {
+					break
+				}
 
-			if i == ptr {
-				ptr++
+				if !(ptr >= len(headsToCheck)) {
+					ptr = slices.Index(headsToCheck, head.Points) + 1
+					curr = m[headsToCheck[ptr].y][headsToCheck[ptr].x]
+					continue
+				}
+
+				// disableNode(nodeStack.Peek().(*Node))
+				// prev = nodeStack.Peek().(*Node).prev
+				// if prev != nil {
+				// 	nodeStack.Pop()
+				// 	nodeStack.Push(prev)
+
+				// 	head := getHead(prev)
+				// 	ptr = slices.Index(headsToCheck, head.Points) + 1
+				// 	curr = m[headsToCheck[ptr].y][headsToCheck[ptr].x]
+				// 	continue
+				// }
+
+				// head = nodeStack.Pop().(*Node)
+
+				// if nodeStack.Len() == 0 {
+				// 	break
+				// }
+
+				// ptr = slices.Index(headsToCheck, head.Points) + 1
+				// curr = m[headsToCheck[ptr].y][headsToCheck[ptr].x]
+				// continue
+
+				for ptr >= len(headsToCheck) {
+					disableNode(nodeStack.Peek().(*Node))
+					prev = nodeStack.Peek().(*Node).prev
+					if prev != nil {
+						nodeStack.Pop()
+						nodeStack.Push(prev)
+
+						head := getHead(prev)
+						ptr = slices.Index(headsToCheck, head.Points) + 1
+						continue
+					}
+
+					head = nodeStack.Pop().(*Node)
+
+					if nodeStack.Len() == 0 {
+						break
+					}
+
+					ptr = slices.Index(headsToCheck, head.Points) + 1
+				}
+				if nodeStack.Len() == 0 {
+					break
+				}
+
+				curr = m[headsToCheck[ptr].y][headsToCheck[ptr].x]
+				continue
 			}
 
 			//get next head
-			nextHead := heads[ptr]
+			nextHead := headsToCheck[ptr]
 			curr = m[nextHead.y][nextHead.x]
 			ptr++
-
-			//check end game
-			if isGG(currentX, currentY) {
-				return nil
-			}
 		}
 	}
 
 	return fmt.Errorf("no solution")
 }
 
-func exhaustNode(node *Node, currentX, currentY []int) *Node {
-	boom := isBoom(currentX[node.x]+1, currentY[node.y]+1, node.x, node.y)
+func printStack() {
+	stackCopy := *nodeStack
+	for range stackCopy.Len() {
+		fmt.Println(stackCopy.Pop().(*Node).Points)
+	}
+}
+
+func disableNode(node *Node) {
+	//just to be safe
+	if node.active {
+		currentX[node.x]--
+		currentY[node.y]--
+	}
+	node.active = false
+}
+
+func getHead(node *Node) *Node {
+	if node.prev == nil {
+		return node
+	}
+	return getHead(node.prev)
+}
+
+func exhaustNode(node *Node) *Node {
+	boom := isBoom(node.x, node.y)
 	if boom {
 		return node.prev
 	} else {
 		node.active = true
-		m[node.y][node.x] = node
 		currentX[node.x]++
 		currentY[node.y]++
 		if node.next == nil {
 			return node
 		}
 
-		return exhaustNode(node.next, currentX, currentY)
+		return exhaustNode(node.next)
 	}
 }
 
-func isGG(currentX, currentY []int) bool {
+func isGG() bool {
+	xScore := make([]int, len(targetX))
+	yScore := make([]int, len(targetY))
+	for _, row := range m {
+		for _, node := range row {
+			if node != nil && node.active {
+				xScore[node.x]++
+				yScore[node.y]++
+			}
+		}
+	}
+
 	for i := range currentX {
-		if currentX[i] != targetX[i] || currentY[i] != targetY[i] {
+		if xScore[i] != targetX[i] || yScore[i] != targetY[i] {
 			return false
 		}
 	}
@@ -150,19 +240,20 @@ func isGG(currentX, currentY []int) bool {
 	return true
 }
 
-func isBoom(valX, valY, indexX, indexY int) bool {
-	if valX > targetX[indexX] || valY > targetY[indexY] {
+func isBoom(indexX, indexY int) bool {
+	if currentX[indexX]+1 > targetX[indexX] || currentY[indexY]+1 > targetY[indexY] {
 		return true
 	}
 
 	return false
 }
 
-func printMap(m [][]*Node) {
+func printMap() {
 	//println color
-	// red := "\033[31m"
-	// Reset = "\033[0m"
+	red := "\033[31m"
+	reset := "\033[0m"
 	fmt.Printf("     ")
+
 	for _, val := range targetX {
 		fmt.Printf(" %v  ", val)
 	}
@@ -179,15 +270,22 @@ func printMap(m [][]*Node) {
 		fmt.Printf("  %v ┋", targetY[i])
 		for _, node := range row {
 			if node != nil {
+				if node.active {
+					fmt.Print(red)
+				}
 				if node.prev != nil {
 					if node.prev.y == node.y {
-						fmt.Print(" ━ ┋")
-					} else {
-						fmt.Print(" | ┋")
+						fmt.Print(" ━ ")
+					} else if node.prev.x == node.x {
+						fmt.Print(" | ")
 					}
 				} else {
-					fmt.Print(" o ┋")
+					fmt.Print(" o ")
 				}
+				if node.active {
+					fmt.Print(reset)
+				}
+				fmt.Print("┋")
 			} else {
 				fmt.Print("   ┋")
 			}
@@ -199,4 +297,36 @@ func printMap(m [][]*Node) {
 		fmt.Print("┉┉┉┉")
 		fmt.Println("┉")
 	}
+}
+
+func initData() {
+	pts := strings.Split(raw, ";")
+	rawNodes := strings.Split(pts[0], ",")
+
+	var prev Points
+	for _, rawNode := range rawNodes {
+		nodePts := strings.Split(rawNode, " ")
+		y, _ := strconv.Atoi(nodePts[1])
+		x, _ := strconv.Atoi(nodePts[2])
+
+		if nodePts[0] == "start" {
+			heads = append(heads, Points{y: y, x: x})
+		} else {
+			relations = append(relations, r{this: prev, to: Points{y: y, x: x}})
+		}
+		prev = Points{y: y, x: x}
+	}
+
+	xtargets := strings.Split(pts[1], ",")
+	for _, xtarget := range xtargets {
+		x, _ := strconv.Atoi(xtarget)
+		targetX = append(targetX, x)
+	}
+	ytargets := strings.Split(pts[2], ",")
+	for _, ytarget := range ytargets {
+		y, _ := strconv.Atoi(ytarget)
+		targetY = append(targetY, y)
+	}
+
+	N = len(targetX)
 }
